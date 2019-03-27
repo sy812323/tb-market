7ac03061372b87d95a3cbce1dbcb193e4e6da694
shangyou <317742988@qq.com>
2019-03-27 14:53:04 +0800

    这次提交了没有评论的商品详情页面

--- js/goodsDetail.js
+++ js/goodsDetail.js
@@ -15,3 +15,23 @@ $(function(){
         spaceBetween : 0//slide之间的距离（单位px）
 	  })
 });
+$(function(){
+	$(".tb-market-content").scroll(function(){
+		let temp = $(".tb-market-content").scrollTop();
+		if(Math.ceil(temp)>120){
+			$("header").css("background","white");
+			$(".tb-market-header-center").css("display","-webkit-flex");
+			$(".tb-market-header-left a").css("background-color","white");
+			$(".tb-market-header-right a").css("background-color","white");
+			$(".tb-market-header-left a i").css("color","#999");
+			$(".tb-market-header-right a i").css("color","#999");
+		}else{
+			$("header").css("background","linear-gradient(rgba(0,0,0,.15),transparent)");
+			$(".tb-market-header-center").css("display","none");
+			$(".tb-market-header-left a").css("background-color","rgba(0,0,0,.4)");
+			$(".tb-market-header-right a").css("background-color","rgba(0,0,0,.4)");
+			$(".tb-market-header-left a i").css("color","white");
+			$(".tb-market-header-right a i").css("color","white");
+		}
+	});
+});
